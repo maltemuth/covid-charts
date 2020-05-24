d94a50c847400eaba8cd84bb0db80538371f11e3
Malte Muth <malte.muth+git-commit@googlemail.com>
2020-05-24 17:59:50 +0200

    fix(lib) only retry 429 and 5xx responses

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,6 +1,6 @@
 const fetchAndRetry = (url: string): Promise<Response> =>
   fetch(url).then((response) => {
-    if (response.status > 400) {
+    if (response.status > 500  || response.status === 429) {
       console.log(`Response was ${response.status} for ${url}, retrying...`);
       return new Promise((resolve) => setTimeout(resolve, 1000)).then(() =>
         fetchAndRetry(url)
