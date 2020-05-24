24ae08041c219aabccd7557f1f92e4354dd396a0
Malte Muth <malte.muth+git-commit@googlemail.com>
2020-05-24 20:01:12 +0200

    feat(chart): remove magic numbers

--- components/CountryDetailsChart.tsx
+++ components/CountryDetailsChart.tsx
@@ -1,9 +1,19 @@
 import { CountryDetails } from "../model/countryDetails/CountryDetails";
-import { COLOR_ACTIVE, COLOR_CONFIRMED, COLOR_RECOVERED, COLOR_DECEASED } from "../model/countryDetails/colors";
+import {
+  COLOR_ACTIVE,
+  COLOR_CONFIRMED,
+  COLOR_RECOVERED,
+  COLOR_DECEASED,
+} from "../model/countryDetails/colors";
 
 interface CountryDetailsChartProps {
   details: CountryDetails[];
 }
+
+const CHART_CANVAS_MARGIN_HORIZONTAL = 10;
+const CHART_CANVAS_MARGIN_VERTICAL = 5;
+const CHART_ASPECT_RATIO = 16 / 7;
+const CHART_MOVING_AVERAGE_FRAME = 14;
 
 interface LinesProps {
   values: number[];
@@ -17,15 +27,22 @@ interface LinesProps {
 }
 
 const xCalculator = (width: number) => (index: number): number =>
-  width - index * 10;
+  CHART_CANVAS_MARGIN_HORIZONTAL +
+  ((width - index) / width) * (width - 2 * CHART_CANVAS_MARGIN_HORIZONTAL);
 
 const yCalculator = (height: number, max: number) => (value: number): number =>
-  height - (value * height) / max;
+  -CHART_CANVAS_MARGIN_VERTICAL +
+  height -
+  (value / max) * (height - 2 * CHART_CANVAS_MARGIN_VERTICAL);
 
 const movingAverage = (frame: number) => (values: number[]): number[] =>
   values.map((_, index) => {
     const start = Math.max(0, index - Math.floor(frame / 2));
     const end = Math.min(values.length, index + Math.floor(frame / 2) + 1);
+
+    if (end === values.length || start === 0) {
+      return null;
+    }
 
     const valuesInFrame = values.slice(start, end);
 
@@ -58,15 +75,23 @@ const Lines: React.FunctionComponent<LinesProps> = ({
           <>
             {includeCircles && (
               <circle
-                r={4}
+                r={30 / width}
                 stroke={color}
+                strokeWidth={15 / width}
                 fill="transparent"
                 cx={x + 1}
                 cy={y}
               ></circle>
             )}
-            {includeLines && typeof values[index - 1] !== "undefined" && (
-              <line x1={x} y1={y} x2={xBefore} y2={yBefore} stroke={color} />
+            {includeLines && typeof values[index - 1] === "number" && (
+              <line
+                x1={x}
+                y1={y}
+                x2={xBefore}
+                y2={yBefore}
+                stroke={color}
+                strokeWidth={15 / width}
+              />
             )}
           </>
         );
@@ -78,8 +103,8 @@ const Lines: React.FunctionComponent<LinesProps> = ({
 const CountryDetailsChart: React.FunctionComponent<CountryDetailsChartProps> = ({
   details,
 }) => {
-  const width = details.length * 10;
-  const height = (width / 16) * 7;
+  const width = details.length;
+  const height = width / CHART_ASPECT_RATIO;
 
   const values = ["active", "confirmed", "recovered", "deceased"].reduce(
     (numbers, prop) => ({
@@ -97,7 +122,7 @@ const CountryDetailsChart: React.FunctionComponent<CountryDetailsChartProps> = (
   const averages = ["active", "confirmed", "recovered", "deceased"].reduce(
     (numbers, prop) => ({
       ...numbers,
-      [prop]: movingAverage(14)(values[prop]),
+      [prop]: movingAverage(CHART_MOVING_AVERAGE_FRAME)(values[prop]),
     }),
     {}
   ) as {
@@ -123,88 +148,101 @@ const CountryDetailsChart: React.FunctionComponent<CountryDetailsChartProps> = (
   const maximum = Math.max(...Object.values(maxima));
 
   return (
-    <svg viewBox={`-20 -20 ${width + 40} ${height + 40}`}>
-      <Lines
-        key="values.active"
-        height={height}
-        offset={0}
-        color={COLOR_ACTIVE}
-        width={width}
-        values={values.active}
-        maximum={maximum}
-      />
-      <Lines
-        key="values.confirmed"
-        height={height}
-        offset={1}
-        color={COLOR_CONFIRMED}
-        width={width}
-        values={values.confirmed}
-        maximum={maximum}
-      />
-      <Lines
-        key="values.recovered"
-        height={height}
-        offset={2}
-        color={COLOR_RECOVERED}
-        width={width}
-        values={values.recovered}
-        maximum={maximum}
-      />
-      <Lines
-        key="values.deceased"
-        height={height}
-        offset={3}
-        color={COLOR_DECEASED}
-        width={width}
-        values={values.deceased}
-        maximum={maximum}
-      />
-      <Lines
-        key="averages.active"
-        height={height}
-        offset={0}
-        color={COLOR_ACTIVE}
-        width={width}
-        values={averages.active}
-        includeCircles={false}
-        includeLines={true}
-        maximum={maximum}
-      />
-      <Lines
-        key="averages.confirmed"
-        height={height}
-        offset={1}
-        color={COLOR_CONFIRMED}
-        width={width}
-        values={averages.confirmed}
-        includeCircles={false}
-        includeLines={true}
-        maximum={maximum}
-      />
-      <Lines
-        key="averages.recovered"
-        height={height}
-        offset={2}
-        color={COLOR_RECOVERED}
-        width={width}
-        values={averages.recovered}
-        includeCircles={false}
-        includeLines={true}
-        maximum={maximum}
-      />
-      <Lines
-        key="averages.deceased"
-        height={height}
-        offset={3}
-        color={COLOR_DECEASED}
-        width={width}
-        values={averages.deceased}
-        includeCircles={false}
-        includeLines={true}
-        maximum={maximum}
-      />
-    </svg>
+    <>
+      <svg viewBox={`0 0 ${width} ${height}`}>
+        <Lines
+          key="values.active"
+          height={height}
+          offset={0}
+          color={COLOR_ACTIVE}
+          width={width}
+          values={values.active}
+          maximum={maximum}
+        />
+        <Lines
+          key="values.confirmed"
+          height={height}
+          offset={1}
+          color={COLOR_CONFIRMED}
+          width={width}
+          values={values.confirmed}
+          maximum={maximum}
+        />
+        <Lines
+          key="values.recovered"
+          height={height}
+          offset={2}
+          color={COLOR_RECOVERED}
+          width={width}
+          values={values.recovered}
+          maximum={maximum}
+        />
+        <Lines
+          key="values.deceased"
+          height={height}
+          offset={3}
+          color={COLOR_DECEASED}
+          width={width}
+          values={values.deceased}
+          maximum={maximum}
+        />
+        <Lines
+          key="averages.active"
+          height={height}
+          offset={0}
+          color={COLOR_ACTIVE}
+          width={width}
+          values={averages.active}
+          includeCircles={false}
+          includeLines={true}
+          maximum={maximum}
+        />
+        <Lines
+          key="averages.confirmed"
+          height={height}
+          offset={1}
+          color={COLOR_CONFIRMED}
+          width={width}
+          values={averages.confirmed}
+          includeCircles={false}
+          includeLines={true}
+          maximum={maximum}
+        />
+        <Lines
+          key="averages.recovered"
+          height={height}
+          offset={2}
+          color={COLOR_RECOVERED}
+          width={width}
+          values={averages.recovered}
+          includeCircles={false}
+          includeLines={true}
+          maximum={maximum}
+        />
+        <Lines
+          key="averages.deceased"
+          height={height}
+          offset={3}
+          color={COLOR_DECEASED}
+          width={width}
+          values={averages.deceased}
+          includeCircles={false}
+          includeLines={true}
+          maximum={maximum}
+        />
+      </svg>
+      <style jsx>{`
+        caption {
+          width: 100vw;
+          display: inline-block;
+          text-align: center;
+        }
+      `}</style>
+      <caption>
+        circle: actual values from table below &mdash; lines: {CHART_MOVING_AVERAGE_FRAME}-day moving
+        average
+      </caption>
+    </>
   );
 };
 
